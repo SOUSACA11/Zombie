70c01aa9256cbb2bb60f8f98c2ffee51238fb208
JINNY <sousaca05@gmail.com>
2023-06-27 12:43:27 +0900

    230627
    건 스크립트 추가

--- Zombie/Assets/01.Scripts/Gun.cs
+++ Zombie/Assets/01.Scripts/Gun.cs
@@ -41,30 +41,79 @@ public class Gun : MonoBehaviour //총 자체의 기능 구현
         fireTransform = transform.GetChild(3); //1차원
     }
 
-    private void OnEnable() //총 상태 초기화 /컴포넌트 활성화.비활성화
+    private void OnEnable() //총 상태 초기화 /컴포넌트 활성화시 발동
     {
         ammoRemain = gunData.startAmmoRemain; //전체 예비 탄알 양을 초기화
+        magAmmo = gunData.magCapacity; //현재 탄창을 가득 채우기
+
+        state = State.Ready; //총의 현재 상태를 총을 쏠 준비가 된 상태로 변경
+        lastFireTime = 0; //마지막으로 총을 쏜 시점을 초기화
 
     }
 
     public void Fire() //발사 시도 /현재 발사 가능한 상태인지 점검(총알이 있나 없나)
     {
+      if (state == State.Ready && Time.time >= lastFireTime + gunData.timeBetFire)
+        // 현재 발사 가능한 상태 &&(이면서) 마지막 총 발사 시점에서 gunData.timeBetFire 이상의 시간이 지남
+        {
+            lastFireTime = Time.time; //마지막 총 발사 시점 갱신
 
+            Shot(); //실제 발사 처리 실행
+        }
     }
 
     private void Shot() //실제 발사 처리 /발사 가능한 상태일경우 발사
     {
+        RaycastHit hit; //레이캐스트에 의한 충돌 정보를 저장하는 컨테이너
+        Vector3 hitPosition = Vector3.zero; //탄알이 맞은 곳을 저장할 변수
 
+        if (Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))
+         //레이캐스트(시작 지점, 방향, 충돌 정보 컨테이너, 사정거리)
+        {
+           
+            //레이가 어떤 물체와 충돌한 경우
+
+            IDamageble target = hit.collider.GetComponent<IDamageble>();
+            //충돌한 상대방으로부터 IDamageble 오브젝트 가져오기 시도
+
+            if (target  != null) //상대방으로부터 IDamageble 오브젝트를 가져오는 데 성공했다면
+            {
+                target.OnDamage(gunData.damage, hit.point, hit.normal);
+                //상대방의 OnDamage 함수를 실행시켜 상대방에 대미지 주기
+            }
+
+            hitPosition = hit.point; //레이가 충돌한 위치 저장
+        }
+
+        else //레이가 다른 물체와 충돌하지 않았다면
+        {
+            hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
+            //탄알이 최대 사정거리까지 날아갔을 때의 위치를 충돌 위치로 사용
+        }
+
+        StartCoroutine(ShotEffect(hitPosition)); //발사 이펙트 재생 시작
+
+        magAmmo--; //남은 탄알 수를 -1
+        if(magAmmo <= 0)
+        {
+            state = State.Empty; //탄창에 남은 탄알이 없다면 총의 현재 상태를 Empty로 갱신
+        }
     }
 
     private IEnumerator ShotEffect(Vector3 hitPosition) //발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
     {
+        muzzleFlashEffect.Play(); //총구 화염 효과 재생
+        shellEjectEffext.Play(); //탄피 배출 효과 재생
 
+        gunAudioplayer.PlayOneShot(gunData.shotClip); //총격 소리 재생
+
+        bulletLineRenderer.SetPosition(0, fireTransform.position); //선의 시작점은 총구의 위치
+        bulletLineRenderer.SetPosition(1, hitPosition); //선의 끝점은 입력으로 들어온 충돌 위치
         bulletLineRenderer.enabled = true; //라인 렌더러를 활성화하여 탄알 궤적을 그림
 
         yield return new WaitForSeconds(0.03f); //0.03초 동안 잠시 처리를 대기
                        //WaitForEndOfFrame - 예외처리(조건식) 상황에서 적용 /한프레임만 넘기겠다
-
+       
         bulletLineRenderer.enabled = false; //라인 렌더러를 비활성화하여 탄알 궤적을 지움
     }
 
